71dc18e04d2f0f7c89c475253072aed91305f923
yusufakcil <yusufakcil58@gmail.com>
2023-01-31 21:15:59 -0500

    New Changes done on second class

--- src/Day2GitHub/SecondGitHubClass.java
+++ src/Day2GitHub/SecondGitHubClass.java
@@ -3,6 +3,7 @@ package Day2GitHub;
 public class SecondGitHubClass {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub");
+        System.out.println("I am creating conflict");
         System.out.println("2+2");
     }
 }
